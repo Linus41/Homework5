02109ca17dfef269ea5961365f0c5633c598b9c5
Linus41 <schief.craft@gmail.com>
2020-03-11 18:32:44 -0700

    added assignment README

--- script.js
+++ script.js
@@ -15,6 +15,13 @@ date.innerHTML = nowDate.format("MMM Do YYYY");
 // make onclick event for class "save"
 var input = document.getElementById("input9");
 
-$(".save").on("click", function () {
-    localStorage.setItem("#input9", input.value);
+// save to local storage
+  $(".save").on("click", function () {
+    localStorage.setItem("#input9", JSON.stringify(input.value));
 })
+// get item from local storage
+function grab() {
+    if(localStorage != null);
+    var final = localStorage.getItem("#input9");
+    input = final;
+}
